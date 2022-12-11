947fc906427462a18530b7f2739e8b6b5b727d0f
Muhammad Abbas <abbas@Muhammads-MacBook-Pro.local>
2022-12-11 22:15:14 +0400

    animal dog relationship

---
+++ src/main/java/com/esafzay/animal/Animal.java
@@ -0,0 +1,14 @@
+package com.esafzay.animal;
+
+public abstract class Animal {
+
+    private final String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/esafzay/animal/Dog.java
@@ -0,0 +1,8 @@
+package com.esafzay.animal;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+}
